ffd663bcf20d6cd9beae25212784e079f7dc1b2f
Edward Alder <eddy.alder@hotmail.com>
2026-03-02 22:10:36 -0800

    Initial Commit

---
+++ SpaceLabelsV2/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  SpaceLabelsV2
+//
+//  Created by Edward Alder on 3/2/26.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
